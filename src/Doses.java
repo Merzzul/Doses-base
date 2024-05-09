a4b30af1bbcb7f0d6c0af2323bb778a927e5e8c0
IvanMovchanets <iv.movchanets@gmail.com>
2024-05-09 17:36:04 +0300

    get rid of sout with emtpy string

--- src/Doses.java
+++ src/Doses.java
@@ -1,11 +1,11 @@
 class Doses {
     private int dose;
 
-    public void setDose(int d){
-    dose =d;
+    public void setDose(int d) {
+        dose = d;
     }
 
-    public int getDose(){
+    public int getDose() {
         return dose;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-        System.out.println("Головне меню програми:");
-        System.out.println("1. Додати дозу");
-        System.out.println("2. Переглянути сумарну накопичену дозу");
-        System.out.println("3. Видалити сумарну накопичену дозу");
-        System.out.println("4. Закрити програму");
-        System.out.print("Зробіть свій вибір: ");
-        System.out.println("");
+            System.out.println("Головне меню програми:");
+            System.out.println("1. Додати дозу");
+            System.out.println("2. Переглянути сумарну накопичену дозу");
+            System.out.println("3. Видалити сумарну накопичену дозу");
+            System.out.println("4. Закрити програму");
+            System.out.print("Зробіть свій вибір:\n");
 
             int choice = scanner.nextInt();
             switch (choice) {
@@ -21,11 +20,9 @@ public class Main {
                     System.out.println("Введіть отриману дозу");
                     int dose = scanner.nextInt();
                     doseBase.setDose(dose);
-                    System.out.println("");
                     break;
                 case 2:
-                    System.out.println("Сумарна накопичена доза: " + doseBase.getDose() );
-                    System.out.println("");
+                    System.out.println("Сумарна накопичена доза: " + doseBase.getDose() + "\n");
                     break;
                 case 3:
                     System.out.println("Сумарна накопичена доза буде видалена, ви впевненні?");
@@ -35,22 +32,20 @@ public class Main {
                         case "ТАК":
                             doseBase.setDose(0);
                             System.out.println("Сумарна накопичена доза була видалена.");
-                            System.out.println("Сумарна накопичена доза: " + doseBase.getDose() );
+                            System.out.println("Сумарна накопичена доза: " + doseBase.getDose() + "\n");
                             break;
                         case "НІ":
                             System.out.println("Сумарна накопичена доза не була видалена.");
-                            System.out.println("Сумарна накопичена доза: " + doseBase.getDose() );
+                            System.out.println("Сумарна накопичена доза: " + doseBase.getDose() + "\n");
                             break;
                         default:
-                            System.out.println("Невірний вибір. Будь ласка, введіть ТАК чи НІ");
+                            System.out.println("Невірний вибір. Будь ласка, введіть ТАК чи НІ\n");
                     }
-                    System.out.println("");
                     break;
                 case 4:
                     return;
                 default:
-                    System.out.println("Невірний вибір. Будь ласка, введіть номер від 1 до 4.");
-                    System.out.println("");
+                    System.out.println("Невірний вибір. Будь ласка, введіть номер від 1 до 4.\n");
             }
         }
     }
